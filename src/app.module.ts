1e3551d67d332646f47de8f638959df75411b29d
root <root@cod-2-souaneaic.lan>
2026-08-24 21:58:56 +0000

    Creation du module building et health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BuildingsModule } from './buildings/buildings.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
+  imports: [BuildingsModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
